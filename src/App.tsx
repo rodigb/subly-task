5ba7a4a8f733a698525d0fe7db3b4c5fef3f18fa
rodigb <rodix08@gmail.com>
2022-07-09 20:47:45 +0100

    working cards with API information

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,23 @@ import {Card} from './components/cards'
 
 import './App.css';
 
-var results: any;
+
 
 const App: React.FC = () => {
 
+  //create a function to fetch the api results and assign them.
 
-  const [value,setValue]=React.useState([]);
+  const [value,setValue]= React.useState<any[]>([])
   console.log(value)
   React.useEffect(()=>{
 
     async function fetchResults(){
 
-      const response = await fetch('https://run.mocky.io/v3/a811c0e9-adae-4554-9694-173aa23bc38b')//fetch api url
-      //console.log(response)
-      const jsonData= await response.json();
-      //console.log(jsonData.media);
+      const response:(any)= await fetch('https://run.mocky.io/v3/a811c0e9-adae-4554-9694-173aa23bc38b')//fetch api url
+      const jsonData:(any)= await response.json();
+      const mainInfo:(any)= jsonData.media
 
-      setValue(jsonData.media)
+      setValue(mainInfo)
 
       }
     fetchResults();
@@ -33,11 +33,10 @@ const App: React.FC = () => {
         <h1>Subly Cards!</h1>
         {value.map((values)=>{
           return(
-            <Card coverImage="hello" title="hello" status = "hello"></Card>
+            <Card coverImage={values.cover} title={values.name} status ={values.status}></Card> //get's the card component and fills them with the api results.
           )
         })}
-        
- 
+
       </header>
     </div>
   )
