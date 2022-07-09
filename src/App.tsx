cadc1da60818fd6d5f3f998478e649fb54f7d76f
rodigb <rodix08@gmail.com>
2022-07-09 19:04:28 +0100

    fetched url data and created JSON. cards generated depending on how many results appear from the JSON.

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,41 @@ import {Card} from './components/cards'
 
 import './App.css';
 
-fetch('https://run.mocky.io/v3/a811c0e9-adae-4554-9694-173aa23bc38b')//fetch api url
-  .then(response => response.json())
-  .then(data => console.log(data));
-
-
+var results: any;
 
 const App: React.FC = () => {
+
+
+  const [value,setValue]=React.useState([]);
+  console.log(value)
+  React.useEffect(()=>{
+
+    async function fetchResults(){
+
+      const response = await fetch('https://run.mocky.io/v3/a811c0e9-adae-4554-9694-173aa23bc38b')//fetch api url
+      //console.log(response)
+      const jsonData= await response.json();
+      //console.log(jsonData.media);
+
+      setValue(jsonData.media)
+
+      }
+    fetchResults();
+  },[])
+
+
+
   return(
     <div className="App">
       <header className = "App-header">
         <h1>Subly Cards!</h1>
-        <Card coverImage="hello" title="hello" status = "hello"></Card>
-        <Card coverImage="hello" title="hello" status = "hello"></Card>
-        <Card coverImage="hello" title="hello" status = "hello"></Card>
+        {value.map((values)=>{
+          return(
+            <Card coverImage="hello" title="hello" status = "hello"></Card>
+          )
+        })}
+        
+ 
       </header>
     </div>
   )
