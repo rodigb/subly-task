303091890c8f4115cc274f6ec24097ad9fe9d7eb
rodigb <rodix08@gmail.com>
2022-07-10 16:38:49 +0100

    select dropdown first stage

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,47 @@ import errorImg from './images/error.png'
 import {TranscribingCard} from './components/transcribingCard'
 
 import './App.css';
- 
- 
+
+let opt:string;
  
 
 const App: React.FC = () => {
 
+
+
+  function dropValue(){ //function to get the value of the dropdown option
+    var dropdownID = (document.getElementById("dropdown") as HTMLSelectElement)
+    var select = dropdownID.selectedIndex;
+    opt = dropdownID.options[select].value;
+    console.log(opt)
+
+    if(opt.includes("any")){
+      console.log("any selected")
+
+
+    }
+    if(opt.includes("en")){
+      console.log("en selected")
+
+
+    }
+    if(opt.includes("nl")){
+      console.log("nl selected")
+
+
+    }
+    if(opt.includes("cz")){
+      console.log("cz selected")
+
+      
+    }
+  }
+
+
   //create a function to fetch the api results and assign them.
 
   const [value,setValue]= React.useState<any[]>([])
-  
+
   console.log(value)
   React.useEffect(()=>{
 
@@ -29,17 +60,10 @@ const App: React.FC = () => {
       //    console.log(mainInfo[i].languages)
       // }
 
-     
-    
-
       }
-       
+
     fetchResults();
   },[])
-
- 
-
-  
 
 
 
@@ -47,13 +71,24 @@ const App: React.FC = () => {
     <div className="App">
       <header className = "App-header">
         <h1>Subly Cards!</h1>
+        </header>
+        <body>
+          <select id='dropdown' onChange={dropValue}>
+            <option id = 'option' value ='any'>Any</option>
+            <option id = 'option' value ='en'>EN</option>
+            <option id ='option' value='nl'>NL</option>
+            <option id = 'option' value='cz'>CZ</option>
+            </select>
         {value.map((values)=>{
+
+
           //declaring variables for values from value.map(api results)
           var cover = values.cover
           var title = values.name
           var status = values.status
           var languages = values.languages
-          console.log(status)
+ 
+
 
           if(status == "error" || null){ //action for cards with errors
             cover = errorImg
@@ -70,8 +105,9 @@ const App: React.FC = () => {
             <Card coverImage={cover} title={values.name} status ={values.status} lastUpdated = "" languages={languages+" " }></Card> //get's the card component and fills them with the api results.
           )
         })}
+        </body>
 
-      </header>
+      
     </div>
   )
 };
